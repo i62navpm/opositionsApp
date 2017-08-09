1a4b490db902ba1a5a0c4ff826d72ca8914e569e
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-09 20:05:17 +0200

    Install dependencies and create stores/utils

---
+++ src/store/auth.js
@@ -0,0 +1,19 @@
+// var debug = require('debug')('auth')
+
+// import AWSCognitoSDK from '../utils/cognito.service.js'
+
+export default {
+  namespaced: true,
+  state: {
+
+  },
+  getters: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+}
---
+++ src/utils/cognito.service.js
@@ -0,0 +1,9 @@
+const debug = require('debug')('utils:cognitoSDK')
+// const AWSCognito = require('aws-sdk/clients/cognitoidentity')
+
+export default class AWSCognitoSDK {
+  constructor () {
+    debug('Init AWSCognitoSDK')
+  }
+}
+
