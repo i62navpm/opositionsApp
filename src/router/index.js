cd2e81e6962e59d9659115ae10648fed5d880cbf
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-10 22:49:26 +0200

    New verification and login routers

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Register from '@/components/Register'
+import Verification from '@/components/Verification'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -16,6 +18,16 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/verification',
+      name: 'verification',
+      component: Verification
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
