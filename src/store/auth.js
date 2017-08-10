0b4dadb8a7acb9b1b8bfde40f2ab7ce179e43f96
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-10 22:50:44 +0200

    Register, verification, login in auth services

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,21 +1,56 @@
-// var debug = require('debug')('auth')
+var debug = require('debug')('auth')
 
 import AWSCognitoSDK from '@/utils/cognito.service.js'
+import router from '@/router'
 
 export default {
   namespaced: true,
   state: {
-    congitoSDK: null
+    congitoSDK: null,
+    user: null
   },
   getters: {
 
   },
   mutations: {
-    SET_COGNITO(state, cognito) {
-      state.congitoSDK = cognito
-    }
+    SET_COGNITO: (state, cognito) => (state.congitoSDK = cognito),
+    SET_USER: (state, user) => (state.user = user)
   },
   actions: {
-    INIT_COGNITO: ({commit}) => commit('SET_COGNITO', new AWSCognitoSDK())
+    INIT_COGNITO: ({ commit }) => commit('SET_COGNITO', new AWSCognitoSDK()),
+    REGISTER_USER: ({ commit, state }, { email, password }) => {
+      state.congitoSDK.registerUser({ email, password }, (err, result) => {
+        if (err) {
+          debug('Error:', err)
+          return
+        }
+
+        commit('SET_USER', result.user.username)
+        !result.userConfirmed && router.push({name: 'verification'})
+        debug('User registered correctly')
+      })
+    },
+    VERIFICATE_CODE: ({ commit, state }, code) => {
+      state.congitoSDK.verificateCode({username: state.user, code}, (err, result) => {
+        if (err) {
+          debug('Error:', err)
+          return
+        }
+
+        !result.userConfirmed && router.push({name: 'login'})
+        debug('User verified correctly')
+      })
+    },
+    LOGIN_USER: ({ commit, state }, { email, password }) => {
+      state.congitoSDK.loginUser({ username: state.user, email, password }, {
+        onSuccess: (result) => {
+          console.log('access token + ' + result.getAccessToken().getJwtToken())
+          console.log(result)
+          router.push({name: 'hello'})
+          debug('User logged correctly')
+        },
+        onFailure: (err) => debug('Error:', err)
+      })
+    }
   }
 }
