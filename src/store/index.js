ce057ff5c979bb54548e76352799e958cef81a5d
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-10 22:49:59 +0200

    New persisted state vuex in local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import createPersistedState from 'vuex-persistedstate'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth.js'
@@ -5,6 +6,7 @@ import auth from './auth.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState({key: 'user', paths: ['auth.user']})],
   strict: true,
   modules: {
     auth
