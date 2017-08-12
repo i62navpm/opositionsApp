748f61558863c800b90d0258ab7fa14cf18f1fd7
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-12 22:16:41 +0200

    Remove comment

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -20,23 +20,6 @@ v-card.grey.lighten-5(flat)
             v-btn(small flat primary :to="'forgotPassword'") Forgot the password?
             v-btn(small flat primary :to="'Register'") You don't have an account yet?
 
-  //- v-container(fluid)
-  //-   v-layout(row wrap)
-  //-     v-flex(xs12 md6 offset-md3)
-  //-       v-card
-  //-         v-toolbar.indigo(dark)
-  //-           v-toolbar-title Login
-  //-         v-card-text
-  //-           form(@keyup.enter="sendLogin")
-  //-             v-text-field(type="email" label="Email", v-model="email", required)
-  //-             v-text-field(type="password" label="Password", hint="The length must be more than 8 characters", v-model="password" required)
-  //-             v-layout(row justify-space-between)
-  //-               small *indicates required field
-  //-               v-btn(info :loading="loading"  @click.prevent="sendLogin" :disabled="loading") Sign in
-  //-             v-alert(error dismissible transition="scale-transition" v-model="alert") {{error}}
-  //-           v-layout(row-sm column child-flex-sm justify-space-between)
-  //-             v-btn(small flat primary :to="'forgotPassword'") Forgot the password?
-  //-             v-btn(small flat primary :to="'Register'") You don't have an account yet?
 </template>
 
 <script>
