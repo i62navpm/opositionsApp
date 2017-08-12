153dd815f26bed2636d2acda1aca7ae6b8c96cc2
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-12 21:21:10 +0200

    New login form style

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,21 @@
 <template lang="pug">
-  form
-    div
-      label Email
-      input.input(type='email' placeholder='Enter an email' v-model="email")
-    div
-      label Password
-      input.input(type='password' placeholder='Enter a password' v-model="password")
-    div
-      button(@click.prevent="sendLogin") Sign in
-    div
-      router-link(:to="'forgotPassword'") Forgot the password?
+  v-container(fluid fill-height)
+    v-layout(row wrap)
+      v-flex(xs12 md6 offset-md3)
+        v-card
+          v-toolbar.indigo(dark)
+            v-toolbar-title Login
+          v-card-text
+            form(@keyup.enter="sendLogin")
+              v-text-field(type="email" label="Email", v-model="email", required)
+              v-text-field(type="password" label="Password", hint="The length must be more than 8 characters", v-model="password" required)
+              v-layout(row justify-space-between)
+                small *indicates required field
+                v-btn(info :loading="loading"  @click.prevent="sendLogin" :disabled="loading") Sign in
+              v-alert(error dismissible transition="scale-transition" v-model="alert") {{error}}
+            v-layout(row justify-space-between)
+              v-btn(small flat primary :to="'forgotPassword'") Forgot the password?
+              v-btn(small flat primary :to="'Register'") You don't have an account yet?
 </template>
 
 <script>
@@ -21,7 +27,10 @@ export default {
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      loading: false,
+      alert: false,
+      error: ''
     }
   },
   methods: {
@@ -30,8 +39,17 @@ export default {
     }),
     async sendLogin() {
       debug('Sending login form')
-      await this.loginUser({email: this.email.trim(), password: this.password.trim()})
-      this.$router.push({ name: 'hello' })
+      this.loading = true
+
+      try {
+        await this.loginUser({ email: this.email.trim(), password: this.password.trim() })
+        this.$router.push({ name: 'hello' })
+      } catch (error) {
+        debug('Error:', error.message)
+        this.error = error.message
+        this.alert = true
+      }
+      this.loading = false
     }
   }
 }
