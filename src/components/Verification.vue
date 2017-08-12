b8cadbc56eaea3b8a5f45421ddf787a81ce7818a
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-12 22:08:37 +0200

    Change styles forms

--- src/components/Verification.vue
+++ src/components/Verification.vue
@@ -1,17 +1,20 @@
 <template lang="pug">
-  v-container(fluid)
-    v-layout(row wrap)
-      v-flex(xs12 md6 offset-md3)
-        v-card
-          v-toolbar.indigo(dark)
-            v-toolbar-title Verificate email
-          v-card-text
-            form(@keyup.enter="sendCode")
-              v-text-field(label="Enter the verification code", v-model="code", required)
-              v-layout(row justify-space-between)
-                small *indicates required field
-                v-btn(info :loading="loading"  @click.prevent="sendCode" :disabled="loading") Verificate code
-              v-alert(error dismissible transition="scale-transition" v-model="alert") {{error}}
+v-card.grey.lighten-5(flat)
+  v-toolbar.primary.elevation-3(dark, extended)
+    v-toolbar-title Oposition APP
+  v-layout(row)
+    v-flex(xs12 sm10 offset-sm1 md6 offset-md3)
+      v-card.card--flex-toolbar
+        v-toolbar.white(card, prominent)
+          v-toolbar-title Verificate email
+        v-divider
+        v-card-text
+          form(@keyup.enter="sendCode")
+            v-text-field(label="Enter the verification code", v-model="code", required)
+            v-layout(row justify-space-between)
+              small *indicates required field
+              v-btn(info :loading="loading"  @click.prevent="sendCode" :disabled="loading") Verificate code
+            v-alert(error dismissible transition="scale-transition" v-model="alert") {{error}}
 
 </template>
 
