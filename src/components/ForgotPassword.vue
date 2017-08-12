3f353f98442f8628974a3c4d9232900cb9adf7a3
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-12 21:45:11 +0200

    New forgot password form style

--- src/components/ForgotPassword.vue
+++ src/components/ForgotPassword.vue
@@ -1,18 +1,27 @@
 <template lang="pug">
-  form(v-if="showPasswordForm")
-    div
-      label Code
-        input.input(type='text' placeholder='Enter a code' v-model="code")
-      label Password
-      input.input(type='password' placeholder='Enter a password' v-model="password")
-    div
-      button(@click.prevent="sendActivatePassword") Set password
-  form(v-else)
-    div
-      label Email
-      input.input(type='email' placeholder='Enter an email' v-model="email")
-      div
-        button(@click.prevent="sendForgotPassword") Forgot password
+  v-container(fluid fill-height)
+    v-layout(row wrap)
+      v-flex(xs12 md6 offset-md3)
+        v-card
+          v-toolbar.indigo(dark)
+            v-toolbar-title Forgot the password
+          v-card-text
+            form(v-if="showPasswordForm" @keyup.enter="sendActivatePassword")
+              v-text-field(label="Code", v-model="code", required)
+              v-text-field(type="password" label="Password", hint="The length must be more than 8 characters", v-model="password" required)
+              v-layout(row justify-space-between)
+                small *indicates required field
+                v-btn(info :loading="loading"  @click.prevent="sendActivatePassword" :disabled="loading") Set password
+              v-alert(error dismissible transition="scale-transition" v-model="alert") {{error}}
+            form(v-else @keyup.enter="sendForgotPassword")
+              v-text-field(type="email" label="Email", v-model="email", required)
+              v-layout(row justify-space-between)
+                small *indicates required field
+                v-btn(info :loading="loading"  @click.prevent="sendForgotPassword" :disabled="loading") Forgot password
+              v-alert(error dismissible transition="scale-transition" v-model="alert") {{error}}
+              v-layout(row justify-space-between)
+                v-btn(small flat primary :to="'Login'") Have you an account?
+
 </template>
 
 <script>
@@ -26,7 +35,10 @@ export default {
       showPasswordForm: false,
       email: '',
       code: '',
-      password: ''
+      password: '',
+      loading: false,
+      alert: false,
+      error: ''
     }
   },
   methods: {
@@ -36,12 +48,29 @@ export default {
     }),
     async sendForgotPassword() {
       debug('Sending forgot password form')
-      this.showPasswordForm = await this.forgotPassword(this.email.trim())
+      this.loading = true
+      try {
+        this.showPasswordForm = await this.forgotPassword(this.email.trim())
+        this.alert = false
+      } catch (error) {
+        debug('Error:', error.message)
+        this.error = error.message
+        this.alert = true
+      }
+      this.loading = false
     },
     async sendActivatePassword() {
       debug('Sending confirm password form')
-      await this.confirmPassword({email: this.email.trim(), code: this.code.trim(), password: this.password.trim()})
-      this.$router.push({name: 'login'})
+      this.loading = true
+      try {
+        await this.confirmPassword({ email: this.email.trim(), code: this.code.trim(), password: this.password.trim() })
+        this.$router.push({ name: 'login' })
+      } catch (error) {
+        debug('Error:', error.message)
+        this.error = error.message
+        this.alert = true
+      }
+      this.loading = false
     }
   }
 }
