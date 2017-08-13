54346fdf9b6a7a2e80f846a995977d3c177c0b99
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-13 20:15:50 +0200

    Table pagination, search, with mock lambda

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,89 +4,76 @@
       | English table
       v-spacer
       v-text-field(append-icon='search', label='Search', single-line, hide-details, v-model='search')
-    v-data-table.elevation-1(v-bind:headers='headers', v-bind:items='items', v-bind:search='search', v-bind:pagination.sync='pagination', :total-items='totalItems', :loading='loading')
+    v-data-table.elevation-1(v-bind:headers='headers', v-bind:items='items' v-bind:pagination.sync='pagination', :total-items='totalItems', :loading='loading')
       template(slot='headerCell', scope='props')
         span(v-tooltip:bottom="{ 'html': props.header.text }")
           | {{ props.header.text }}
       template(slot='items', scope='props')
-        td {{ props.item.name }}
-        td.text-xs-right {{ props.item.calories }}
+        td {{ props.item.posicion }}
+        td.text-xs-right {{ props.item.nombre }}
 
 </template>
 
 <script>
+import AWSLambdaMock from '@/utils/lambda.mock'
+
 export default {
   name: 'list',
   data() {
     return {
       search: '',
+      lazySearch: '',
       totalItems: 0,
       loading: true,
       pagination: {},
+      preventWatcher: true,
       headers: [
         {
           text: 'Position',
           align: 'left',
-          sortable: false,
-          value: 'name'
+          sortable: true,
+          value: 'posicion'
         },
-        { text: 'Name', align: 'left', sortable: false, value: 'calories' }
+        { text: 'Name', align: 'left', sortable: true, value: 'nombre' }
       ],
-      items: [
-        {
-          value: false,
-          name: 'Frozen Yogurt',
-          calories: 159
-        },
-        {
-          value: false,
-          name: 'Ice cream sandwich',
-          calories: 237
-        },
-        {
-          value: false,
-          name: 'Eclair',
-          calories: 262
-        },
-        {
-          value: false,
-          name: 'Cupcake',
-          calories: 305
-        },
-        {
-          value: false,
-          name: 'Gingerbread',
-          calories: 356
-        },
-        {
-          value: false,
-          name: 'Jelly bean',
-          calories: 375
-        },
-        {
-          value: false,
-          name: 'Lollipop',
-          calories: 392
-        },
-        {
-          value: false,
-          name: 'Honeycomb',
-          calories: 408
-        },
-        {
-          value: false,
-          name: 'Donut',
-          calories: 452
-        },
-        {
-          value: false,
-          name: 'KitKat',
-          calories: 518
-        }
-      ]
+      items: []
     }
   },
+  watch: {
+    pagination: {
+      async handler() {
+        if (this.preventWatcher) return
+
+        let { data, total } = await this.getDataFromApi()
+        this.items = data
+        this.totalItems = total
+      },
+      deep: true
+    },
+    search: function () {
+      clearTimeout(this.timeout)
+      this.timeout = setTimeout(async () => {
+        let { data, total } = await this.getDataFromApi()
+        this.items = data
+        this.totalItems = total
+      }, 500)
+    }
+  },
+  async mounted() {
+    let { data, total } = await this.getDataFromApi()
+    this.items = data
+    this.totalItems = total
+    this.preventWatcher = false
+  },
   methods: {
+    getDataFromApi: async function () {
+      this.loading = true
+      const { sortBy, descending, page, rowsPerPage } = this.pagination
+      let { data, total } = await AWSLambdaMock(this.search, sortBy, descending, page, rowsPerPage)
+      this.loading = false
+
+      return { data, total }
+    }
   }
 }
 </script>
