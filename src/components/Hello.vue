4ab8636590615927d836edb7534f4cd72258854a
Manuel Navarro Perez <manuel.navarro@gigigo.com>
2017-08-13 14:19:12 +0200

    New cards with options

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,41 +1,48 @@
 <template lang="pug">
-  .hello
-    button(@click.prevent="logout()") Logout
-    h1 {{ msg }}
-    button(@click.prevent="callLambda()") Call lambda
+v-container(fluid style="min-height: 0;" grid-list-lg)
+  v-layout(column wrap)
+    v-flex(sm10 offset-sm1 md6 offset-md3)
+      router-link(:to="'list'")
+        v-card.purple.white--text
+          v-container(fluid, grid-list-lg)
+            v-layout(row)
+              v-flex(xs7)
+                div
+                  .headline English general list
+                  div Search your name in final list ang get your position
+              v-flex(xs5)
+                v-card-media(src='https://i.redd.it/yuw4cb0mfzjx.png', height='125px', contain)
+    //- v-flex(sm10 offset-sm1 md6 offset-md3)
+    //-   v-card.cyan.darken-2.white--text
+    //-     v-container(fluid, grid-list-lg)
+    //-       v-layout(row)
+    //-         v-flex(xs7)
+    //-           div
+    //-             .headline Supermodel
+    //-             div Foster the People
+    //-         v-flex(xs5)
+    //-           v-card-media(src='https://vuetifyjs.com/static/doc-images/cards/desert.jpg', height='125px', contain)
+    //- v-flex(sm10 offset-sm1 md6 offset-md3)
+    //-   v-card.red.white--text
+    //-     v-container(fluid, grid-list-lg)
+    //-       v-layout(row)
+    //-         v-flex(xs7)
+    //-           div
+    //-             .headline Halycon Days
+    //-             div Ellie Goulding
+    //-         v-flex(xs5)
+    //-           v-card-media(src='/static/doc-images/cards/halcyon.png', height='125px', contain)
+
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import AWSLambdaSDK from '@/utils/lambda.service'
-
 export default {
-  name: 'hello',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js PWA',
-      lambda: null
-    }
-  },
-  created() {
-    this.lambda = new AWSLambdaSDK()
-  },
-  methods: {
-    ...mapActions({
-      logoutUser: 'auth/LOGOUT_USER'
-    }),
-    callLambda() {
-      this.lambda.invoke()
-    },
-    async logout() {
-      await this.logoutUser()
-      this.$router.push({ name: 'login' })
-    }
-  }
+  name: 'hello'
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
-
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
 </style>
